11c9be7e3bcd46d7cbcb6ceb39a1a9ed4e366cc9
Talaku123 <69880305+Talaku123@users.noreply.github.com>
2024-02-09 12:00:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+function productCost () {
+        // document.getElementById("js-credit-img").addEventListener("click", () => {
+        
+            const calculateToIt = Math.random((0) * .1) / 100
+
+
+
+       document.getElementById("js-credit-img").innerText= `Total Amount: \n ${calculateToIt}`
+      //  })
+      
+      }   
+
+      
+        
+
+    
